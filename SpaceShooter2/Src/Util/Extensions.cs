ba0c22bd942d1f9050515157dcb75dd0a80d3d18
unset <quinnthepigeon@gmail.com>
2024-11-19 20:23:40 +0100

    make aggressively inlined

--- SpaceShooter2/Src/Util/Extensions.cs
+++ SpaceShooter2/Src/Util/Extensions.cs
@@ -1,8 +1,10 @@
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 
 namespace SpaceShooter2.Src.Util;
 
 internal static class Extensions
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float DeltaTime(this GameTime gameTime) => (float)gameTime.ElapsedGameTime.TotalSeconds;
 }
