e63687077f20e1424be4a97ac6ab7665e7e556b6
unset <quinnthepigeon@gmail.com>
2024-11-26 22:36:01 +0100

    tweak asteroid colours

--- SpaceShooter2/Src/Astroid.cs
+++ SpaceShooter2/Src/Astroid.cs
@@ -19,6 +19,8 @@ internal class Astroid : TexturedGameObject, IUpdate
     public readonly bool unbreakable;   // whether the asteroid can be destroyed by a bullet
     public readonly float radius;       // the radius of the asteroid, includes scale
 
+    // multiplier from 0..1, larger asteroids are lower and smaller asteroids are higher.
+    public float Multiplier => (-1 * transform.scale.X) + Const.MAX_ASTROID_SIZE;
 
     // creates a new astroid with a random position at the top of the screen.
     // there is also a random chance this astroid will be unbreakable
@@ -26,28 +28,32 @@ internal class Astroid : TexturedGameObject, IUpdate
     {
         transform.origin = Vector2.One / 2.0F;  // put the origin in the centre
         transform.rotation = glob.random.NextSingle() * MathF.Tau; //random rotation from 0..TAU
-        transform.scale = Vector2.One * ((glob.random.NextSingle() + 1F) * Const.MAX_ASTROID_SIZE / 2F); //make the scaling random
+        float size = glob.random.NextSingle();
+        transform.scale = Vector2.One * ((size + 1F) * Const.MAX_ASTROID_SIZE / 2F); //make the scaling random
         /* MAX_SIZE is divided by 2 because NextSingle produces a value between 0..1.
            We add 1 to make sure we don't get any zero issues. Making the range 1..2.
            Multiplying by MAX_SIZE then would equate to the Actual max size to be 2 * MAX_SIZE. */
 
         // put the astroid offscreen so the player doesn't see it spawning in
-        transform.position.Y = -(glob.textures.astroid.Width * transform.scale.X);
+        transform.position.Y = -1 * glob.textures.astroid.Width * transform.scale.X;
+
+        radius = (glob.textures.astroid.Width / 2.0F * transform.scale.X) + (glob.textures.bullet.Width / 2.0F); // add the half of the bullet's texture's width, to compensate for
 
         // have a random chance that the astroid is unbreakable (the player needs to dodge these asteroids)
         if (glob.random.NextSingle() < 0.10F) // 10% chance
         {
             unbreakable = true;
-            textureTint = new Color(0xFF888888);    // tint the texture if the asteroid is unbreakable
+            textureTint = new Color(0xFF434343);    // tint the texture if the asteroid is unbreakable
             transform.position.X = glob.player.transform.position.X;
         }
         else
         {
             unbreakable = false;
+            byte val = (byte)(0xAA * Multiplier + 0x55);
+            textureTint = new Color(val, val, val);    // tint the texture if the asteroid is unbreakable
             transform.position.X = glob.random.Next(0, screenWidth);
         }
 
-        radius = (glob.textures.astroid.Width / 2.0F * transform.scale.X) + (glob.textures.bullet.Width / 2.0F); // add the half of the bullet's texture's width, to compensate for
         this.glob = glob;
     }
 
@@ -60,7 +66,7 @@ internal class Astroid : TexturedGameObject, IUpdate
     public void Update()
     {
         // move the astroid down (bigger asteroids move slower)
-        transform.position.Y += ((-1 * transform.scale.X) + Const.MAX_ASTROID_SIZE) * Const.ASTEROID_SPEED * glob.gameTime.DeltaTime();
+        transform.position.Y += Multiplier * Const.ASTEROID_SPEED * glob.gameTime.DeltaTime();
 
         // if the astroid fell off the screen; destroy the astroid
         if (transform.position.Y - radius > Const.SCREEN_HEIGHT)
