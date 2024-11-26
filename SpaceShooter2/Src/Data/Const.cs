d91ca96d8afeae6a32b141839b0177d4c9df851e
unset <quinnthepigeon@gmail.com>
2024-11-26 21:13:50 +0100

    fix whitespace formatting

--- SpaceShooter2/Src/Data/Const.cs
+++ SpaceShooter2/Src/Data/Const.cs
@@ -16,9 +16,9 @@ internal static class Const
     public const float MAX_ASTROID_SIZE = 2F;       // what the maximum size percentage of the astroid is allowed to be.
 
     // texture names
-        public const string TEXTURE_ASTEROID = "astroid";
-        public const string TEXTURE_BULLET = "bullet";
-        public const string TEXTURE_SPACESHIP_0 = "spaceship/spaceship_0";
-        public const string TEXTURE_SPACESHIP_1 = "spaceship/spaceship_1";
-        public const string TEXTURE_SPACESHIP_2 = "spaceship/spaceship_2";
+    public const string TEXTURE_ASTEROID = "astroid";
+    public const string TEXTURE_BULLET = "bullet";
+    public const string TEXTURE_SPACESHIP_0 = "spaceship/spaceship_0";
+    public const string TEXTURE_SPACESHIP_1 = "spaceship/spaceship_1";
+    public const string TEXTURE_SPACESHIP_2 = "spaceship/spaceship_2";
 }
