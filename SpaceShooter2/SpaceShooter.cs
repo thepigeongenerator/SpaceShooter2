7ee4b7dd5a91a3f6da50d16eb0e2150a563d0eb3
unset <quinnthepigeon@gmail.com>
2024-11-29 23:28:09 +0100

    move BinarySerializer to a standalone project

--- SpaceShooter2/SpaceShooter.cs
+++ SpaceShooter2/SpaceShooter.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using SpaceShooter2.Src;
 using SpaceShooter2.Src.Data;
 using SpaceShooter2.Src.Util;
@@ -9,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using ThePigeonGenerator.Util;
 
 namespace SpaceShooter2;
 public partial class SpaceShooter : Core.Game
